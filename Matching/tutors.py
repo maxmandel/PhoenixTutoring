ed5c1cf43283cfddfe6f2bd94fcf4d48c4a31b3b
Max Mandel <max.tribeca@gmail.com>
2023-11-27 02:04:27 -0600

    comments, changes to tutor initialization

--- Matching/tutors.py
+++ Matching/tutors.py
@@ -1,7 +1,9 @@
 
 class Tutor:
     """
-    A class to represent a tutor applicant. Includes attributes inputted as:
+    A class to represent a tutor applicant. 
+    
+    Includes attributes inputted as:
     inputLst: List[str]
     - Name: str
     - Year: str
@@ -23,23 +25,31 @@ class Tutor:
     """
 
     def __init__(self, inputLst):
-        self.name = inputLst[0]
-        self.year = int(inputLst[1])
-        self.email = inputLst[2]
-        self.phone = inputLst[3]
+        self.name = inputLst[0] # str name
+        self.year = int(inputLst[1]) # int year (can compare w/ others via <>=)
+        self.email = inputLst[2] # str email (to be used for contact info directory)
+        self.phone = inputLst[3] # ^
 
         subjects = inputLst[4]
-        sblst = subjects.split(",")
+        sblst = subjects.split(",") # list: [subj1, ranking1, s2, r2, ...]
         sbdict = {}
         for i in range(len(sblst)):
             if (i % 2 != 0):
                 continue
             else:
-                sbdict[sblst[i]] = int(sblst[i+1])
-        
+                sbdict[sblst[i]] = int(sblst[i+1]) # subject-ranking dictionary
         self.subjects = sbdict
-        grades = inputLst[5]
-        self.grades = grades.split(",")
+
+        grades = inputLst[5].split(",")
+        glst = []
+        for group in grades: # e.g. "1-4"
+            a = int(group[0]) # int("1") = 1
+            b = int(group[2]) # int("4") = 4
+            for gval in range(a, b+1):
+                if gval not in glst:
+                    glst.append(gval)
+        self.grades = glst # e.g. ["1-4", "9-12"] -> [1, 2, 3, 4, 9, 10, 11, 12]
+
         availablity = inputLst[6]
         self.available = availablity.split(",")
     
@@ -48,23 +58,13 @@ class Tutor:
         return len(self.subjects.keys())
     
     def can_tutor(self, subj: str) -> bool:
-
+        # make sure we're checking a real subject
         assert(subj in self.subjects.keys())
         return (not (self.subjects[subj] == 0))
     
     def available_on(self, day:str) -> bool:
 
         return (day in self.available)
-    
-    def ok_grades(self):
-        res = []
-        for range in self.grades:
-            low = int(range[0])
-            high = int(range[2])
-            for grade in range(low, high+1):
-                if grade not in res:
-                    res.append(grade)
-        return res
 
 def makeTutor(inputLst):
     if len(inputLst) == 7:
