70d20bc94eb8016fc383028097b3c32ce75ede4c
Max Mandel <max.tribeca@gmail.com>
2023-12-23 14:22:27 -0500

    matching functions changes

--- Matching/matching.py
+++ Matching/matching.py
@@ -7,9 +7,35 @@ def intersection(lst1, lst2):
     return lst3
 
 def possible_match(t: Tutor, s: Tutee)->bool:
+    """
+    Checks if there's any availability and subject overlap. If there is, returns 
+    a tuple of lists of the overlapping times and subjects, otherwise returns 
+    False.
+    """
 
-    if (len(intersection(t.available, s.available)) == 0):
+    poss_times = intersection(t.available, s.available)
+    poss_subjects = intersection(t.allsubjects, s.allsubjects)
+
+    if (len(poss_times) == 0):
         return False
-    elif (len(intersection(t.subjects, s.subjects)) == 0):
+    elif (len(poss_subjects) == 0):
         return False
     
+    return (poss_times, poss_subjects)
+
+def no_match(ts, ss):
+    """
+    Considering lists of tutors and tutees, determines if there is any one tutee
+    who does not "cleanly" match with any tutors.
+    ts: List[Tutor]
+    ss: List[Tutee]
+    """
+    no_matches = []
+    for s in ss:
+        counter = 0
+        for t in ts:
+            if type(possible_match(t, s)) == tuple:
+                counter += 1
+                break
+        if counter == 0:
+            no_matches.append(s)
